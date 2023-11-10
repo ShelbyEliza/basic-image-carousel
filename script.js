5f10fee366eac2edca6d73a48c711d478d79e797
ShelbyEliza <shelbyelizashipley@gmail.com>
2023-11-10 11:19:21 -0800

    Add timer to image transitions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const buttons = document.querySelectorAll('[data-carousel-button]');
+const circles = document.querySelector('[data-circles]');
+const slides = document.querySelector('[data-slides]');
 
 buttons.forEach((button) => {
 	button.addEventListener('click', () => {
@@ -8,6 +10,7 @@ buttons.forEach((button) => {
 			.querySelector('[data-slides]');
 
 		const activeSlide = slides.querySelector('[data-active]');
+		const activeCircle = circles.querySelector('[data-active]');
 
 		// converts to an array:
 		let newIndex = [...slides.children].indexOf(activeSlide) + offset;
@@ -17,7 +20,27 @@ buttons.forEach((button) => {
 		if (newIndex >= slides.children.length) {
 			newIndex = 0;
 		}
+		circles.children[newIndex].dataset.active = true;
+		delete activeCircle.dataset.active;
+
 		slides.children[newIndex].dataset.active = true;
 		delete activeSlide.dataset.active;
 	});
 });
+/** TODO: Add timeout progression */
+function progressSlides() {
+	const activeSlide = slides.querySelector('[data-active]');
+	const activeCircle = circles.querySelector('[data-active]');
+	let newIndex = [...slides.children].indexOf(activeSlide) + 1;
+
+	if (newIndex >= slides.children.length) {
+		newIndex = 0;
+	}
+	circles.children[newIndex].dataset.active = true;
+	delete activeCircle.dataset.active;
+
+	slides.children[newIndex].dataset.active = true;
+	delete activeSlide.dataset.active;
+}
+
+setInterval(progressSlides, 5000);
